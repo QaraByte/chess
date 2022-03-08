acdb63b2bae1204b5ae378afdd10ed4a5ec319e0
QaraByte <pchelpkz16@gmail.com>
2022-03-08 18:19:15 +0600

    1) Указал для всех ячеек координаты в атрибуте "id"; 2) Прописал логику для ладьи, чтобы разрешенные ячейки выделялись классом .can-move.

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
         let img = $(this).find('img');
         if (img.length > 0) {
             $('.clicked').removeClass('clicked');
+            $('.can-move').removeClass('can-move');
             $(this).addClass('clicked');
             //console.log($(this).attr('id'));
             $('td').css('background-color', '');
@@ -23,7 +24,7 @@ $(document).ready(function() {
             let id = $(this).attr('id');
             console.log(id);
             let figureAttr = $(this).data('figure');
-            console.log(figure);
+            console.log(figureAttr);
             if (figureAttr == 'rook-white') {
                 let x = id[1];
                 let y = id[3];
@@ -31,10 +32,23 @@ $(document).ready(function() {
                 let figure = {};
                 figure.x = x;
                 figure.y = y;
+
                 let tds = $('td');
                 for (let i = 0; i < tds.length; i++) {
                     let td_id = $(tds[i]).attr('id');
-                    console.log(td_id);
+                    if (td_id != undefined) {
+                        //console.log(td_id);
+                        if (figure.x < 8 && td_id[3] == figure.y) {
+                            if (td_id[1] > figure.x) {
+                                $(tds[i]).addClass('can-move');
+                            }
+                        }
+                        if (figure.y > 0 && td_id[1] == figure.x) {
+                            if (td_id[1] < figure.y) {
+                                $(tds[i]).addClass('can-move');
+                            }
+                        }
+                    }
                 }
 
             } else if (figureAttr == 'knight-white') {
